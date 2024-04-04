6f481af96a776963790f7846a0b0f28ed6417505
Dennis RF <148806422+drogerzf@users.noreply.github.com>
2024-04-04 14:56:48 -0300

    1st commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const db = require('./connection');
+const bodyParser = require('body-parser');
+
+
+
+const PORT = 3000; 
+
+app.listen(PORT, function() {
+    console.log(`o express está na porta ${PORT}`);
+});
+
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+//db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("conectou ao banco com sucesso");
+    })
+    .catch(err => {
+        console.log('ocorreu um erro de conexao', err);
+    });
+
+//routes    
+app.get("/", (req, res) => {
+    res.send(`está funcionando na porta ${PORT}`)
+});
---
+++ routes/rjob.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Jobs');
+
+// add job via post
+router.post('/add', (req, res) => {
+    let = {TITULO, SALARY, COMPANY, EMAIL, NEW } = req.body
+})
